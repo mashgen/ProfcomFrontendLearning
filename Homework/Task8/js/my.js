826a1dcdd8156428e36ddb3d3b8ff9bf5504d9a8
Mary Kulakova <manyakulakova@gmail.com>
2020-10-19 21:29:49 +0300

    js Task8, commit1

---
+++ Homework/Task8/js/my.js
@@ -0,0 +1,4 @@
+let products = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum", "Salt Caramell", "Sweet Vanilla"];
+let last = products.length - 1;
+let recent = products[last];
+alert (recent);
